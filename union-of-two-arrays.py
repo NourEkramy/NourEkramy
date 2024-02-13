03717239e27ee927b542503da93433d983c66869
Nour Ekramy <98906756+NourEkramy@users.noreply.github.com>
2024-02-13 16:53:14 +0200

    Create union-of-two-arrays.py

---
+++ union-of-two-arrays.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+class Solution:    
+    #Function to return the count of number of elements in union of two arrays.
+    def doUnion(self,a,n,b,m):
+        
+        #code here
+        uni=a+b
+        un=set(uni)
+        return len(un)
